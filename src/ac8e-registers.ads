5c456a2f37b4ec25fb108ea66b4a505df63b9b7a
Anuj Seth <anujseth76@gmail.com>
2023-04-08 20:41:59 +0530

    add register definitions

--- src/ac8e-registers.ads
+++ src/ac8e-registers.ads
@@ -1,11 +1,18 @@
+with Ac8e.Random_Access_Memory;
+
 package Ac8e.Registers is
-   subtype Eight_Bit_Type is Natural range 0 .. 255;
-   subtype Sixteen_Bit_Type is Natural range 0 .. 2**16 - 1; 
-   subtype V_Register_Identifier is Natural range 0 .. 15;
+   package RAM renames Ac8e.Random_Access_Memory;
 
-   procedure Set_V (X : V_Register_Identifier; 
-                    Value : Eight_Bit_Type);
+   subtype Zero_To_Fifteen is Natural range 0 .. 15;
 
-   Vx : array (V_Register_Identifier) of Eight_Bit_Type;
-   I : Sixteen_Bit_Type;
+   procedure Set_V (X : Zero_To_Fifteen; 
+                    Value : Byte);
+
+private
+   Vx : array (Zero_To_Fifteen) of Byte;
+   I : RAM.Memory_Location;
+   PC : RAM.Memory_Location;
+   SP : Zero_To_Fifteen;
+   Stack : array (Zero_To_Fifteen) of RAM.Memory_Location;
+   Delay_Timer, Sound_Timer : Byte; 
 end Ac8e.Registers;
