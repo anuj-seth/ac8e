1479bff49cd81282244781a95f6838023012b71d
Anuj Seth <anujseth76@gmail.com>
2023-05-12 22:01:16 +0530

    remove redundant instruction creator package

--- src/ac8e-instruction_creator.ads
+++
@@ -1,6 +0,0 @@
-with Ac8e.Instruction;
-
-package Ac8e.Instruction_Creator is
-   function Create (Op : Machine_Code)
-      return Ac8e.Instruction.Instruction_Type'Class;
-end Ac8e.Instruction_Creator;
